8735474c0fd81166f6f2295a56ad508ec4ba0ccc
Pham ngoc diep <n08ni.dieppn@gmail.com>
2021-02-19 23:43:02 +0700

    feat: Improve Dockerfile frontend to get better perfomance when build.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost/goals');
+        const response = await fetch('http://backend-node-app:3000/goals');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals', {
+      const response = await fetch('http://backend-node-app:3000/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost/goals/' + goalId, {
+      const response = await fetch('http://backend-node-app:3000/goals/' + goalId, {
         method: 'DELETE',
       });
 
