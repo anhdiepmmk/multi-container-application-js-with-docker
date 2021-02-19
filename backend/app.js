2459553f66f9ab5f0bb4e977c01f5f0ff8cef134
Pham ngoc diep <n08ni.dieppn@gmail.com>
2021-02-19 23:36:00 +0700

    feat: update cli and change port at the backend-node-app

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://localhost:27017/course-goals',
+  'mongodb://mongodb1:27017/course-goals',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -95,7 +95,7 @@ mongoose.connect(
       console.error(err);
     } else {
       console.log('CONNECTED TO MONGODB');
-      app.listen(80);
+      app.listen(3000);
     }
   }
 );
